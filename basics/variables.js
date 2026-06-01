ec2735ad122594e6cf8e5362f6c2d7fe8e478a38
swornim11 <swornimmaharjan020@gmail.com>
2026-06-01 17:26:37 +0545

    variable commit

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const account_id=22332
+let accountemail="swornimmaharjan@gmail"
+let accountpass="1234"
+accountcity="lalitpur"
+
+accountemail="swornim@email.com"
+accountpass="9876"
+accountcity="khokana"
+// value declare garera xodyo vane tesko output undefined aauxa
+
+console.log(account_id);
+// yesle chai table dinxa
+
+console.table([account_id,accountemail,accountpass, accountcity])
+/*aba yesko vitra jati cmt lekheeni hunxaa
+until and unless end nagare samma
+var use nagarum
+block scope ra functional scope ma problem aauxa
+*/
+
