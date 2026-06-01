778f40944afb9cf5477c7d2c96e969bbd401d99a
swornim11 <swornimmaharjan020@gmail.com>
2026-06-01 17:56:33 +0545

    data type commit

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+// alert(abuii)  yo node js ma chaldaina browser ma matra chalxa
+console.log("Mr.swornim maharjan")
+console.log(23+34)
+
+let name="swornim"
+let age=19
+let isloggedin= false  //boolean
+
+/*
+number
+bigint yo chaii dheraii integer use garne bela use hunxa
+string= ""
+boolean => true/false
+null; yesto ni garna mildo raixaaa, yesle chaii empty xa vanera bujhauxa
+undefined
+symbol; unique
+*/
+//object
+console.log(typeof("swornim")) //yesle chai yo k ho vanera dekhauxa jastai int string bla bla..
+console.log(typeof(null)) //sabai data type ma use hunxa
+console.log(typeof undefined) 
