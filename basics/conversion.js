3ab5f48f1177fd762452955e539fe17ae4fbf9aa
swornim11 <swornimmaharjan020@gmail.com>
2026-06-01 20:37:28 +0545

    conversion commit

---
+++ basics/conversion.js
@@ -0,0 +1,26 @@
+let score="23"
+console.log(typeof score);
+
+//string lai number ma convert garne
+//string or any other data_type
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+//example: number to boolean
+let a=1
+let b=Boolean(a)
+console.log(b);
+/* if string ma kei lekhyo vane
+boolean ma 1 auxa 
+but string empty xa vane boolean will be 0*/
+
+//example2: number to string
+let c=213
+let d=String(c)
+console.log(d);
+//number tei dekhauxa ouput ma but sting ma chai onvert vaisakeko hunxa
+console.log(typeof d);
+
+
+
+
