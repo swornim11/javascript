8c89fd145861490a945f7e4d92277bb31a624089
swornim11 <swornimmaharjan020@gmail.com>
2026-06-02 19:54:40 +0545

    commit change

--- basics/datatype.js
+++ basics/datatype.js
@@ -1,3 +1,4 @@
+//primitive datatype
 // alert(abuii)  yo node js ma chaldaina browser ma matra chalxa
 console.log("Mr.swornim maharjan")
 console.log(23+34)
@@ -20,3 +21,34 @@ console.log(typeof("swornim")) //yesle chai yo k ho vanera dekhauxa jastai int s
 console.log(typeof(null)) //sabai data type ma use hunxa
 console.log(typeof undefined) 
 console.log(typeof 123)
+
+//Big int 
+let bigint=2835262763827n
+console.log(bigint);
+//lamo number ko paxadi n rakhyo vane datatype automatic big int hunxa
+//else error aauxa
+
+const id=Symbol('123')
+console.log(typeof id); //Symbol
+console.log(id);
+
+
+
+//Reference datatype (non-primitive datatype)
+//array ,objects, function
+
+const tt=["dogesh","kirish"];
+let obj={  //let garda ni hunxa nagarda ni hunxa
+    name: "swornim",
+    age:19,
+}
+
+//function
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+console.log(myFunction);
+console.log(typeof myFunction);
+//non-primitive ko datatype sabai ko function nai auxa
+
