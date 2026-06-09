df3c65c126134706137565668288342313441fc4
swornim11 <swornimmaharjan020@gmail.com>
2026-06-09 20:31:11 +0545

    commit

---
+++ basics/life.js
@@ -0,0 +1,11 @@
+//Immediately invoked function expression(IIFE)
+
+(function apple(){
+    console.log(`DGCONNECTED `);
+})();
+
+(function apple1(name){
+    console.log(`DGCONNECTED ${name}`);
+})('swornim');
+
+    
