f2720c0f5c940efd5d40f978801a7f7069026448
swornim11 <swornimmaharjan020@gmail.com>
2026-06-10 15:57:25 +0545

    commy

---
+++ controlflow/one.js
@@ -0,0 +1,34 @@
+//if
+/*
+if(true){
+    //yo vitra ko execute hunxa
+
+}
+if(false){
+    //yo vitra ko execute hudaina
+}
+const userlog=true
+if (userlogin){
+
+}
+*/
+//of else sabaii c ko jastai
+const month=3
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+        case 2:
+            console.log("feb");
+            break;
+
+            case 3:
+                console.log("mar");
+                break;
+                default:
+                    console.log("defauilt case match");
+                    break;
+                    
+            
+
+}
