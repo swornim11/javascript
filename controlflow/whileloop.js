660a0c0d53c34bb30e857ae8bb5757efe1842a22
swornim11 <swornimmaharjan020@gmail.com>
2026-06-11 19:35:54 +0545

    loop commit

---
+++ controlflow/whileloop.js
@@ -0,0 +1,20 @@
+let a=0
+while(a<=10){
+    console.log(`the number is ${a}`);
+    a =a+2
+    
+}
+let array=['ram','hari','shyam']
+let arr=0
+while(arr<array.length){
+    console.log(array[arr]);
+    arr=arr+1
+}
+
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+    
+} while (score<=10);
