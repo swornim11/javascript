4b9d1847913cac53421480d6e6eabba2429d21d1
swornim11 <swornimmaharjan020@gmail.com>
2026-07-22 19:29:32 +0545

    project

---
+++ projects/p_one.md
@@ -0,0 +1,11 @@
+# Project related to DOM
+
+## project link
+[click here]()
+
+# solution code
+## Project 1
+```javascript
+console.log("swornim")
+```
+
---
+++ projects/project1/col.js
@@ -0,0 +1,32 @@
+const buttons= document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){ //button ma hune kam
+    console.log(button)
+    button.addEventListener('mouseover',function(e){//event, k kam garne ho dekhauxa
+        //yesma maila click ko satta mouseover halyo vane curser le xoko color aauxa
+      console.log(e);
+      console.log(e.target);
+      if(e.target.id ==='grey'){
+        body.style.backgroundColor = e.target.id; //simply 'grey'
+      }//yesle chaii grey color ma thichyo vane screen grey color ma convert gardinxa
+      //so similarly
+      if(e.target.id ==='white'){
+        body.style.backgroundColor = e.target.id; //simply 'white'
+      }if(e.target.id ==='blue'){
+        body.style.backgroundColor = e.target.id; //simply 'blue'
+      }if(e.target.id ==='yellow'){
+        body.style.backgroundColor = e.target.id; //simply 'yellow'
+      }
+      if(e.target.id ==='red'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id ==='pink'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id ==='green'){
+        body.style.backgroundColor = e.target.id;
+      }
+
+    })
+});
